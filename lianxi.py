0d788750d7436f4a8d8e44d51030bd4ebbe4d8ea
jingcedongjian <jingcedongjian@gmail.com>
2026-08-18 22:25:46 +0800

    test feature branch

---
+++ lianxi.py
@@ -0,0 +1,30 @@
+logs = [
+    {"api": "/login", "status": 200},
+    {"api": "/login", "status": 500},
+    {"api": "/user", "status": 200},
+    {"api": "/user", "status": 200},
+    {"api": "/user", "status": 404},
+    {"api": "/order", "status": 500},
+    {"api": "/order", "status": 500},
+]
+def lowest_success_api(logs):
+    apps:dict[dict]={}
+    apps2={}
+    
+    for log in logs:
+        
+        if log["api"] not in apps:
+            
+        
+            apps[log["api"]]={"total":0,
+                              "success":0}
+        apps[log["api"]]["total"]+=1 
+        if log["status"]==200:
+            apps[log["api"]]["success"]+=1
+    for key in apps.keys():
+        apps2[key]=0
+
+
+#www 
+    return apps2
+print(lowest_success_api(logs))
